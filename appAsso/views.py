21107ce653e69151fd37e47ecbcd20f6273c68ff
eddys999 <eddy20003@gmail.com>
2023-02-03 15:29:30 +0100

    eddy finalisation de la page des stats

--- appAsso/views.py
+++ appAsso/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate, get_user_model
 from django.contrib import messages
 from .models import Products #import de la base product venant du modele 
+from django.db.models import Sum
 # Create your views here.
 
 
@@ -18,8 +19,39 @@ def base(request):
     return render(request, "base.html")
 
 def index(request):
-    count = Products.objects.all().count()
-    context = {'count': count}
+    count_all_items = Products.objects.all().count()
+    count_boxe_items = Products.objects.filter(cProduit = "boxe").count()
+    count_basket_items = Products.objects.filter(cProduit = "basket").count()
+    count_foot_items = Products.objects.filter(cProduit = "foot").count()
+    ##### valeur totale du stok 
+    items = Products.objects.all()
+    valeur_totale_stock = sum(items.values_list('prix', flat=True))
+
+    ##### prix moyen du stock
+    prix_moyen = valeur_totale_stock / count_all_items
+
+    ##### valeur du stock des articles de boxe 
+    boxe_items = Products.objects.filter(cProduit = "boxe")
+    valeur_stock_boxe = sum(boxe_items.values_list('prix', flat=True))
+    ##### valeur du stock des articles de foot
+    foot_items = Products.objects.filter(cProduit = "foot")
+    valeur_stock_foot = sum(foot_items.values_list('prix', flat=True))
+    ##### valeur du stock des articles de basket 
+    basket_items = Products.objects.filter(cProduit = "basket")
+    valeur_stock_basket = sum(basket_items.values_list('prix', flat=True))
+
+
+    context = {'count_all_items': count_all_items, 
+                'items':items,
+                'count_boxe_items': count_boxe_items, 
+                'count_basket_items': count_basket_items,
+                'count_foot_items': count_foot_items,
+                'valeur_totale_stock': float(valeur_totale_stock),
+                'prix_moyen':float(prix_moyen), 
+                'valeur_stock_boxe': float(valeur_stock_boxe), 
+                'valeur_stock_foot': float(valeur_stock_foot), 
+                'valeur_stock_basket': float(valeur_stock_basket),
+                }
     return render(request, "acceuil.html", context)
 
 
